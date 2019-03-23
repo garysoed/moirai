c095de918c37b00882fec883d24e6f5f092ba19a
Gary Soedarsono <garysoed@gmail.com>
2019-03-23 11:34:28 -0700

    Stringify error

---
+++ src/stringify-error.ts
@@ -0,0 +1,16 @@
+import { Verbosity } from './verbosity';
+
+export function stringifyError(target: Error, verbosity: number):
+    string {
+  switch (verbosity) {
+    case Verbosity.NONE:
+      return '';
+    case Verbosity.QUIET:
+      return target.message;
+    default:
+      return [
+        target.message,
+        target.stack,
+      ].join('\n');
+  }
+}
