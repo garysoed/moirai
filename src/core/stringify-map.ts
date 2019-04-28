741f2222ee5a18ef7e6f03a718fd352d5003cd6f
Gary Soedarsono <garysoed@gmail.com>
2019-04-28 13:15:07 -0700

    left behind

---
+++ src/core/stringify-map.ts
@@ -0,0 +1,21 @@
+import { Verbosity } from './verbosity';
+
+type StringifyFn = (target: any, verbosity: number) => string;
+
+export function stringifyMap(
+    target: Map<unknown, unknown>,
+    verbosity: number,
+    stringify: StringifyFn,
+): string {
+  if (verbosity === Verbosity.NONE) {
+    return '';
+  }
+
+  const stringifiedProperties = [...target]
+      .map(([key, value]) => {
+        return `${stringify(key, verbosity - 1)}: ${stringify(value, verbosity - 1)}`;
+      })
+      .join('; ');
+
+  return `Map({${stringifiedProperties}})`;
+}
