632b49dfea70d2d8a602c1fb5d97e77c4257d19b
Gary Soedarsono <garysoed@gmail.com>
2019-06-02 11:35:12 -0700

    lint

--- src/tag/tag-debug.ts
+++ src/tag/tag-debug.ts
@@ -1,7 +1,7 @@
 import { stringify } from '../core/stringify';
 import { Verbosity } from '../core/verbosity';
 
-export function tagDebug(literals:  TemplateStringsArray, ...placeholders: any[]): string {
+export function tagDebug(literals: TemplateStringsArray, ...placeholders: any[]): string {
   let output = '';
   for (let i = 0; i < placeholders.length; i++) {
     output += `${literals[i]}${stringify(placeholders[i], Verbosity.DEBUG)}`;
